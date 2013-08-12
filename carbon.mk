ed0445f809be6f7b24351d6fddd39b7ab172204f
Ravi Kiran P <ravikirancg1@gmail.com>
2013-08-12 23:44:51 +0530

    Support for Edison

---
+++ carbon.mk
@@ -0,0 +1,43 @@
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_hdpi
+
+# Inherit device configuration for Droid Atrix 2.
+$(call inherit-product, device/motorola/edison/full_edison.mk)
+
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_gsm.mk)
+
+# languages
+PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cm_edison
+PRODUCT_BRAND := ATT
+PRODUCT_DEVICE := edison
+PRODUCT_MODEL := MB865
+PRODUCT_MANUFACTURER := MOTO
+PRODUCT_RELEASE_NAME := MOTOROLA ATRIX 2
+PRODUCT_SFX := umts
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
+
+UTC_DATE := $(shell date +%s)
+DATE := $(shell date +%Y%m%d)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=${PRODUCT_DEVICE}_${PRODUCT_SFX} \
+   BUILD_NUMBER=${DATE} \
+   TARGET_DEVICE=${PRODUCT_DEVICE} \
+   BUILD_FINGERPRINT=${PRODUCT_BRAND}/${PRODUCT_DEVICE}_${PRODUCT_SFX}/${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}:${PLATFORM_VERSION}/${BUILD_ID}/${DATE}:user/release-keys \
+   PRIVATE_BUILD_DESC="${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}-user ${PLATFORM_VERSION} ${BUILD_ID} ${DATE} release-keys" \
+   PRODUCT_BRAND=${PRODUCT_BRAND} \
+   BUILD_UTC_DATE= \
+   PRODUCT_DEFAULT_LANGUAGE=en \
+   PRODUCT_DEFAULT_REGION=US \
