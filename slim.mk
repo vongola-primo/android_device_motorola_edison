764e09bcadaa880d71bf4186bbadaae6b4a43099
alteredlikeness <alteredlikeness@gmail.com>
2014-02-02 22:47:47 -0600

    slim setup

--- slim.mk
+++ slim.mk
@@ -1,19 +1,26 @@
+# Release name
+PRODUCT_RELEASE_NAME := edison
+
 # Inherit device configuration for Atrix 2.
 $(call inherit-product, device/motorola/edison/full_edison.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := cm_edison
+PRODUCT_NAME := slim_edison
 PRODUCT_BRAND := ATT
 PRODUCT_DEVICE := edison
 PRODUCT_MODEL := MB865
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := MOTOROLA ATRIX 2
 PRODUCT_SFX := umts
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
 
 UTC_DATE := $(shell date +%s)
 DATE := $(shell date +%Y%m%d)
